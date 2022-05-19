0fb2c2e24143be8fb744226a83ec64f564909313
MayconL27 <mayconleandro2008@gmail.com>
2022-05-19 12:11:18 -0300

    incopleto

---
+++ Desafio02.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Desafio02 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número:");
+        int N = sc.nextInt();
+        for ( int i = 0 ; i <= N ; i++ ) {
+			System.out.println(i);
+            System.out.println(i);	
+		}
+        
+    }
+    
+}
